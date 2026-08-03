f0ca42c92b11a6549ccc17dc96e773e7be3a61c5
ImsalAlwen123 <imsalalwen@gmail.com>
2026-08-04 01:11:23 +0500

    Add files via upload

---
+++ Snake_game.py
@@ -0,0 +1,118 @@
+import pygame
+import random
+
+# Initialize
+pygame.init()
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# Screen size
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+clock = pygame.time.Clock()
+
+# Snake settings
+snake_block = 20
+snake_speed = 10
+
+font = pygame.font.SysFont(None, 35)
+
+def draw_snake(snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, GREEN, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font.render(msg, True, color)
+    screen.blit(mesg, [WIDTH/6, HEIGHT/3])
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = WIDTH / 2
+    y1 = HEIGHT / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_list = []
+    length_of_snake = 1
+
+    foodx = round(random.randrange(0, WIDTH - snake_block) / 20.0) * 20.0
+    foody = round(random.randrange(0, HEIGHT - snake_block) / 20.0) * 20.0
+
+    while not game_over:
+
+        while game_close == True:
+            screen.fill(BLACK)
+            message("Game Over! R dabao dobara khelne ke liye", RED)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        gameLoop()
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        # Deewar se takrao
+        if x1 >= WIDTH or x1 < 0 or y1 >= HEIGHT or y1 < 0:
+            game_close = True
+
+        x1 += x1_change
+        y1 += y1_change
+        screen.fill(BLACK)
+        pygame.draw.rect(screen, RED, [foodx, foody, snake_block, snake_block])
+
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_list.append(snake_head)
+
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+
+        # Khud se takrao
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                game_close = True
+
+        draw_snake(snake_list)
+        pygame.display.update()
+
+        # Khana khaya
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, WIDTH - snake_block) / 20.0) * 20.0
+            foody = round(random.randrange(0, HEIGHT - snake_block) / 20.0) * 20.0
+            length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
